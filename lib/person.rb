6c8942d70ead87f1cbe89f15405b19c363d07510
roy kimathi <kimathiwaweru@gmail.com>
2023-05-24 12:31:33 +0300

    done with lab assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
+    # attr_accessor :name
+    # attr_accessor :job
+    # def initialize (name, job)
+    #     @name = name
+    #     @job = job
+    # end
 end
